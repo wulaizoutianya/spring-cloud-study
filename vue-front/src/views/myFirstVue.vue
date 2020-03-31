265b23dd7b9ccfc46ec3f82a6c13f0d2a736f0cf
raoyiqiang <541440124@qq.com>
2020-03-31 11:23:19 +0800

    this is a spring cloud study project init commit

---
+++ vue-front/src/views/myFirstVue.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="first-app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "myFirstVue",
+    data() {
+      return {
+        msg: "this is my first vue test"
+      }
+    }
+  }
+</script>
---
+++ vue-front/src/views/userLoginCheck.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>账号：<input v-model="userLoginInfoObj.userAccount" :maxlength="75" placeholder="用户账号"/></div>
+    <div>密码：<input v-model="userLoginInfoObj.userPassword" :maxlength="75" placeholder="用户密码"/></div>
+    <div>
+      <Button type="primary" @click="userLoginValidate()">登录</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import urlRouteApi from '@/urlRouteApi'
+
+  export default {
+
+
+    data() {
+      return {
+        userLoginInfoObj: {
+          userAccount: '',
+          userPassword: ''
+        }
+      }
+    },
+    methods: {
+      async userLoginValidate() {
+        alert(this.userLoginInfoObj.userAccount + " ***  " + this.userLoginInfoObj.userPassword + " ** " + urlRouteApi.userLoginCheck);
+
+        this.vueAxios.post(urlRouteApi.userLoginCheck, this.userLoginInfoObj).
+        then(function (response) {
+          console.log(response);
+        }).catch(function (error) {
+          console.log(error);
+        });
+      },
+    }
+  }
+</script>
+<style>
+
+</style>
